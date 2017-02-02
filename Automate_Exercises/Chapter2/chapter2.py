292fe14644f61171c0f90b48470c567753ca58c3
Jayhebe <jayhebe1983@sina.com>
2017-02-02 19:00:33 +0800

    ok

--- Automate_Exercises/Chapter2/chapter2.py
+++ Automate_Exercises/Chapter2/chapter2.py
@@ -1,1 +1,17 @@
 # Control Flow
+
+is_raining = input("Is it raining outside?")
+ 
+if is_raining == "yes":
+    have_umbrella = input("Do you have an umbrella?")
+    if have_umbrella == "yes":
+        print("Let's go!!!")
+    else:
+        print("Let's wait a moment.")
+        is_raining = input("Is it still raining outside?")
+        while is_raining == 'yes':
+            print("Let's wait a moment.")
+            is_raining = input("Is it still raining outside?")
+        print("Let's go!!!")
+else:
+    print("Let's go!!!")
