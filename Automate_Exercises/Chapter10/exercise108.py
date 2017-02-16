21f0ffa56c2c6ea2e882cf21aef0e05d925f9286
Jayhebe <jayhebe1983@sina.com>
2017-02-16 22:15:47 +0800

    2017-02-16

---
+++ Automate_Exercises/Chapter10/exercise108.py
@@ -0,0 +1,29 @@
+import random, logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+guess = ""
+while guess not in ("heads", "tails"):
+    print("Guess the coin toss! Enter heads or tails:")
+    guess = input()
+    
+toss = random.randint(0, 1)
+
+if toss == 0:
+    toss = "heads"
+elif toss == 1:
+    toss = "tails"
+
+if toss == guess:
+    logging.debug("toss is " + toss + ", guess is " + guess)
+    print("You got it!")
+else:
+    logging.debug("toss is " + toss + ", guess is " + guess)
+    print("Nope! Guess again!")
+    guess = input()
+    
+    if toss == guess:
+        logging.debug("toss is " + toss + ", guess is " + guess)
+        print("You got it!")
+    else:
+        logging.debug("toss is " + toss + ", guess is " + guess)
+        print("Nope. You are really bad at this game.")
