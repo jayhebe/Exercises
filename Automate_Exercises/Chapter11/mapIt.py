af222b8e665252097bc05facbd107223aa606829
Jayhebe <jayhebe1983@sina.com>
2017-02-20 21:44:24 +0800

    2017-02-20

--- Automate_Exercises/Chapter11/mapIt.py
+++ Automate_Exercises/Chapter11/mapIt.py
@@ -1,4 +1,11 @@
-import webbrowser, sys
+import requests, bs4
 
-if len(sys.argv) > 1:
-    pass
+# res = requests.get("http://www.baidu.com")
+# res.raise_for_status()
+# baidu = bs4.BeautifulSoup(res.text, "html.parser")
+# print(baidu.select("div"))
+
+exampleFile = open("example.html")
+exampleSoup = bs4.BeautifulSoup(exampleFile, "html.parser")
+# print(type(exampleSoup))
+print(exampleSoup.select("p"))
