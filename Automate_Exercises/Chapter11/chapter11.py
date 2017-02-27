7605288a4394d1920ee06e96dfa467e0ab057b8d
jayhebe <jayhebe1983@sina.com>
2017-02-27 16:43:30 +0800

    2017-02-27

---
+++ Automate_Exercises/Chapter11/chapter11.py
@@ -0,0 +1,11 @@
+import requests
+
+res = requests.get("http://www.gutenberg.org/cache/epub/1112/pg1112.txt")
+
+try:
+    res.raise_for_status()
+    f = open("pg1112.txt", "w", encoding="utf-8")
+    f.write(res.text)
+    f.close()
+except Exception as err:
+    print("There was a problem: %s" % (err))
