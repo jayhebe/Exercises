14e001859d20c072be40af0c8e785a22fd586cd7
Administrator <Administrator@VLABTESTTS01>
2017-05-04 10:58:05 +0200

    2017-05-04

--- Automate_Exercises/Chapter11/lucky.py
+++ Automate_Exercises/Chapter11/lucky.py
@@ -1,5 +1,15 @@
-import webbrowser, sys
+import webbrowser, requests, sys, bs4
 
-print("Baiduing...")
+print("Googling...")
 
-webbrowser.open("http://www.baidu.com/s?wd=" + sys.argv[1])
+if (len(sys.argv) > 1):
+    keywords = " ".join(sys.argv[1:])
+    googleRes = requests.get("http://www.google.com/search?q=" + keywords)
+    googleRes.raise_for_status()
+    
+    googleSoup = bs4.BeautifulSoup(googleRes.text, "html.parser")
+    googleElems = googleSoup.select(".r a")
+    
+    numOpen = min(5, len(googleElems))
+    for i in range(numOpen):
+        webbrowser.open("http://www.google.com/" + googleElems[i].get("href"))
