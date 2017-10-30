8c12d4bbfc9a66ea4725ca59246fdc57cf141dae
Administrator <Administrator@VLABTESTTS01>
2017-10-30 10:05:14 +0100

    2017-10-30

---
+++ Github_Exercises/100+ Python challenging programming/Q51.py
@@ -0,0 +1,4 @@
+try:
+    a = 5 / 0
+except ZeroDivisionError as e:
+    print(e)
---
+++ Github_Exercises/100+ Python challenging programming/Q53.py
@@ -0,0 +1,10 @@
+# email = input("Please input your email: ")
+# 
+# name = email.split("@")[0]
+# 
+# print(name)
+
+# import re
+# 
+# email = input("Please input your email: ")
+# pattern = 
